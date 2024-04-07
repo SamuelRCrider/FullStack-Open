a77fcf477270453d89e0a45132e9f7194bdb72ed
SamuelRCrider <sam.crider@aol.com>
2024-04-07 11:28:42 -0400

    completed exercises 2.16-2.17

--- partTwo/phoneBook/src/App.jsx
+++ partTwo/phoneBook/src/App.jsx
@@ -3,12 +3,15 @@ import Filter from "./components/Filter/component";
 import AddPersonForm from "./components/AddPersonForm/component";
 import PhoneBook from "./components/PhoneBook/component";
 import personsService from "./services/persons";
+import Notification from "./components/Notification/component";
+import "./index.css";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [personFilter, setPersonFilter] = useState("");
+  const [message, setMessage] = useState({ message: null, error: null });
 
   useEffect(() => {
     personsService.getAll().then((allPersons) => {
@@ -40,9 +43,28 @@ const App = () => {
         personsService
           .update(foundPerson.id, changedPersonsNumber)
           .then((updatedPersons) => {
+            setMessage({
+              message: `${updatedPersons.name} has been changed to ${updatedPersons.number}!`,
+              error: null,
+            });
+            setTimeout(() => {
+              setMessage({ message: null, error: null });
+            }, 5000);
             setPersons(
               persons.map((p) => (p.id !== foundPerson.id ? p : updatedPersons))
             );
+            setNewName("");
+            setNewNumber("");
+          })
+          .catch((error) => {
+            setMessage({
+              message: `${object.name} has already been deleted from the phonebook!`,
+              error: error,
+            });
+            setTimeout(() => {
+              setMessage({ message: null, error: null });
+            }, 5000);
+            setPersons(persons.filter((p) => p.id !== foundPerson.id));
             setNewName("");
             setNewNumber("");
           });
@@ -51,6 +73,13 @@ const App = () => {
       // If found person is null, add the new object (new person) to the backend
       // then update the frontend with the addition of the newly created person
       personsService.create(object).then((createdPerson) => {
+        setMessage({
+          message: `${createdPerson.name} has been added to the Phonebook!`,
+          error: null,
+        });
+        setTimeout(() => {
+          setMessage({ message: null, error: null });
+        }, 5000);
         setPersons(persons.concat(createdPerson));
         setNewName("");
         setNewNumber("");
@@ -98,6 +127,7 @@ const App = () => {
         nameValue={newName}
         numberValue={newNumber}
       />
+      <Notification notify={message} />
       <PhoneBook
         personFilter={personFilter}
         filteredPersons={filteredPersons}
